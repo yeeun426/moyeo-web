a55265a9ae7e3d28589bb9e5b42fa749150bffd7
yeeun426 <thsudkcla7@naver.com>
2025-07-21 23:45:51 +0900

    feat : 초기 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
